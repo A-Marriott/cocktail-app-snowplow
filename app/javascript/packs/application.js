fd0743b52eec01f4a44782449fd2a3ab7589f5e6
Alex Marriott <marriottaog@gmail.com>
2021-06-02 12:51:49 +0100

    tracking now operational for a simple track page view, using javascript browser tracker instead of ruby tracker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap';
 
-
+import { newTracker, trackPageView } from '@snowplow/browser-tracker';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,3 +17,8 @@ import 'bootstrap';
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+document.addEventListener('turbolinks:load', () => {
+  newTracker('sp1', 'http://localhost:9090', { appId: 'my-app-id' });
+  trackPageView();
+});
